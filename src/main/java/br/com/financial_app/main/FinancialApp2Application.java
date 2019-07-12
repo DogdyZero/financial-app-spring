f443ebd256f29fe8b1475085d9dcec527d983079
dogdyzero <dhlg_29@hotmail.com>
2019-07-12 17:08:30 -0300

    jwt implementation

--- src/main/java/br/com/financial_app/main/FinancialApp2Application.java
+++ src/main/java/br/com/financial_app/main/FinancialApp2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import br.com.financial_app.repository.UsuarioRepository;
 
--- src/main/java/br/com/financial_app/security/ResourceServerConfig.java
+++ src/main/java/br/com/financial_app/security/ResourceServerConfig.java
@@ -1,30 +1,43 @@
 package br.com.financial_app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+@EnableResourceServer
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().
 		withUser("admin").password("{noop}ads").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected  void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/usuarios/login").permitAll()
 		.anyRequest().authenticated()
 		.and()
-		.httpBasic().and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().csrf().disable();
-		
 	}
+//	@Override
+//	protected void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//		resources.stateless(true);
+//	}
+	
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
