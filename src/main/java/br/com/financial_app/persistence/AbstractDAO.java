5742e61c7f512bb1cbcf67b13d377745a3a73829
dogdyzero <dhlg_29@hotmail.com>
2019-07-26 16:07:41 -0300

    ajuste

--- src/main/java/br/com/financial_app/persistence/AbstractDAO.java
+++ src/main/java/br/com/financial_app/persistence/AbstractDAO.java
@@ -1,34 +1,21 @@
 package br.com.financial_app.persistence;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 
 import br.com.financial_app.domain.EntidadeDominio;
-import br.com.financial_app.domain.Usuario;
-import br.com.financial_app.persistency.queries.FactoryQuery;
-import br.com.financial_app.persistency.queries.IFactoryQuery;
-import br.com.financial_app.persistency.queries.IStrategyQuery;
 import br.com.financial_app.repository.EntidadeRepository;
 
 @Service
 public abstract class AbstractDAO implements IDAO{
-	private int contador =1;
 	protected String tipoConsulta;
-	protected IFactoryQuery fabricaQuery;
 	protected EntidadeDominio entidade;
-	protected SessionFactory sessionFactory;
-	protected Session session;
 	
 	protected EntidadeRepository<?> repository;
-	
 	
 	@Override
 	public String salvar(EntidadeDominio entidade) {
@@ -60,9 +47,34 @@ public abstract class AbstractDAO implements IDAO{
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see br.com.financial_app.persistence.IDAO#consulta(br.com.financial_app.domain.EntidadeDominio)
+	 * o método busca todos os métodos que tem a assinatura semelhante ao getTipoConsulta no repositório
+	 */
 
 	public List<EntidadeDominio> consulta(EntidadeDominio entidade) {
+		
 		List<EntidadeDominio> entidades =  new ArrayList<>();
+		
+		try {
+			Object obj = invokeGenericsMethods();
+			if (obj!=null) {
+				EntidadeDominio e = (EntidadeDominio) obj;
+				entidades.add(e);
+				return entidades;
+			}
+			
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	private Object invokeGenericsMethods() throws NoSuchMethodException, SecurityException, 
+	IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		// pegar a classe que entro repositorio para pegar a interface
 		Class<?> classeRepositorio = repository.getClass();
 		Class<?>[] interfaces = classeRepositorio.getInterfaces();
@@ -78,19 +90,21 @@ public abstract class AbstractDAO implements IDAO{
 		// verificar se na interface tem o nome da classe
 		// OBS: como a consulta é pelo nome da classe, poderá ocorrer problemas, caso exista
 		// classes com nomes iguais em pacotes diferentes
-		// caso não exista o repositorio ocorrerá erro
+		// caso seja métodos herdados da JpaRepositorio, tipo findById / findByAll não será localizado
 		for(Class<?> inter: interfaces) {
 			if(inter.getName().contains(classeEntidade.getSimpleName())) {
 				nomeRepositorio = inter;
 			}
 		}
+		// caso não localize o 
+		if(nomeRepositorio==null)
+			return null;
 		
 		// pegar todos os métodos da classe da entidade
 		Method[] metodos = classeEntidade.getDeclaredMethods();
 		
 		// separar a string de entrada quando encontrar 'And'
-		String[] parametros = param.split("And");
-		parametros = param.split("Or");
+		String[] parametros = param.split("And|Or");
 
 		
 		String[] objMetodos = new String[parametros.length];
@@ -116,34 +130,22 @@ public abstract class AbstractDAO implements IDAO{
 		Object[] invocarMetodos = new Object[parametros.length];
 		i=parametros.length-1;
 		for(String s : objMetodos) {
-			try {
 				Method m = classeEntidade.getMethod(s);
 				invocarMetodos[i] = m.invoke(entidade);
 				i--;
-			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
 		}
 		
-		try {
-			// invocar o método de entrada do repositorio identificado
-			Method method = nomeRepositorio.getMethod(tipoParametro,objType);
-			
-			Object resultado = method.invoke(repository, invocarMetodos);
-			
-			if(resultado !=null) {
-				
-				entidades.add((EntidadeDominio) resultado);
-				return entidades;
-			}
-			
-			return null;
-		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
+		// invocar o método de entrada do repositorio identificado
+		Method method = nomeRepositorio.getMethod(tipoParametro,objType);
 		
+		Object resultado = method.invoke(repository, invocarMetodos);
+		
+		if(resultado !=null) {
+			return resultado;
+		}
 		
 		return null;
+		
 	}
 	
 	public String getTipoConsulta() {
--- src/main/java/br/com/financial_app/persistency/queries/IFactoryQuery.java
+++
@@ -1,8 +0,0 @@
-package br.com.financial_app.persistency.queries;
-
-import br.com.financial_app.domain.EntidadeDominio;
-
-
-public interface IFactoryQuery {
-	public IStrategyQuery createObjQuery(EntidadeDominio entidade);
-}
